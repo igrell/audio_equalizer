35edb4c66a071a0e9e3ca4f620b9a6563e6f520f
igrell <93985679+igrell@users.noreply.github.com>
2024-04-08 23:30:00 +0200

    python part +- done

--- main.py
+++ main.py
@@ -2,25 +2,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-from scipy import signal
-from pydub import AudioSegment
 
 #Import the .wav audio
-f = 'sounds/sine3.wav'
+f = 'sounds/Drone1.wav'
 s, a = wavfile.read(f)
 print('Sampling Rate:', s)
 print('Audio Shape:', np.shape(a))
 
-for i in range(0, 200):
-    print(a[i])
-
 #number of samples
-na = a.shape[0]
+sampleNo = a.shape[0]
 #audio time duration
-la = na / s
+audioLen = sampleNo / s
 
-#plot signal versus time
-t = np.linspace(0, la, na)
+t = np.linspace(0, audioLen, sampleNo)
 plt.plot(t, a, color='purple')
 plt.xlabel('Time (s)')
 plt.show()
+
+if len(a.shape) == 1: # if mono
+    outFile = open("data.txt", "w")
+    outFile.write(str(s))
+    for el in a:
+        outFile.write(str(el))
+        outFile.write('\n')
+    outFile.close()
+elif len(a.shape) == 2:  # if stereo
+    outFileL = open("dataL.txt", "w")
+    outFileR = open("dataR.txt", "w")
+    outFileL.write(str(s))
+    outFileR.write(str(s))
+    for i in range(0, s):
+        outFileL.write(str(a[i, 0]))
+        outFileL.write('\n')
+        outFileR.write(str(a[i, 1]))
+        outFileR.write('\n')
+else:
+    print("Wrong data format")
