49b1bcdb29eafad933c3de2414d7ac134ecbe2dc
igrell <93985679+igrell@users.noreply.github.com>
2024-04-17 23:28:00 +0200

    Plot obtained FFT

---
+++ python_utils/plotTransform.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+from numpy.fft import fft
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    inFile = open('../fft_output.txt')
+    transformData = list(map(float, inFile.read().split()))
+    sampleInterval = float(transformData[0])
+    transformData = transformData[1:]
+    nyquistFreq = 1 / (2 * sampleInterval)
+    t = np.arange(20, 20000, 19980 / len(transformData))
+    plt.plot(t, transformData, color='purple')
+    plt.xlabel('Frequency (Hz)')
+    plt.show()
+    plt.savefig('../transformPlot.png')
